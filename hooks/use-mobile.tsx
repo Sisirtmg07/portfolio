dc5559aacde50e09ff6e8ee9282d504e3fa2f235
Sisirtmg07 <sisirtmg07@gmail.com>
2025-02-03 10:23:46 +0545

    Update use-mobile.tsx

--- hooks/use-mobile.tsx
+++ hooks/use-mobile.tsx
@@ -3,17 +3,34 @@ import * as React from "react"
 const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+  const [isMobile, setIsMobile] = React.useState(() => window.innerWidth < MOBILE_BREAKPOINT)
 
   React.useEffect(() => {
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+    
+    let timeout: NodeJS.Timeout
+    const onChange = (e: MediaQueryListEvent) => {
+      clearTimeout(timeout)
+      timeout = setTimeout(() => setIsMobile(e.matches), 100) // Debounce for Android
     }
+
+    const onResize = () => {
+      clearTimeout(timeout)
+      timeout = setTimeout(() => setIsMobile(window.innerWidth < MOBILE_BREAKPOINT), 100)
+    }
+
     mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
+    window.addEventListener("resize", onResize)
+
+    // Initial check in case of inconsistencies
+    setIsMobile(mql.matches)
+
+    return () => {
+      clearTimeout(timeout)
+      mql.removeEventListener("change", onChange)
+      window.removeEventListener("resize", onResize)
+    }
   }, [])
 
-  return !!isMobile
+  return isMobile
 }
