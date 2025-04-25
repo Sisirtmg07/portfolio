cf28d58378ca4d45563d3c1c152044778daaf60d
Sisirtmg07 <sisirtmg07@gmail.com>
2025-04-25 15:14:56 +0545

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Sisir',
+  description: 'Created with Love',
 }
 
 export default function RootLayout({
